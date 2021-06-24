b7e5039f4f6730fa8f272c8b29ddb4139bd39277
Nhan <nhanbui37@gmail.com>
2021-06-25 00:18:28 +0700

    decorate counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,23 +10,24 @@ class Counter extends Component {
         return (
 
             // grid row bootstrap
-            <div className="row">
+            <div className="container">
+                <div className="row">
 
-                {
-                    /* // column 1 contains getBadgeClassed() and formatCount() 
-                    // use {} to include somethings like js expression
-                    // getBadgeClasses return badge-warning when value is zero, otherwise 
-                    it returns badge-primary
-                    // formatCount returns zero when value is 0, otherwise returns that value
-                    // value of getBadgeClasses is assigned to a span's class, and formatCount 
-                    is assigned to span's text */
-                }
-                <div className="col-1">
-                    <span className={this.getBadgeClasses()}>
-                        {this.formatCount()}</span>
-                </div>
-                <div className="col">
-                    {/* 
+                    {
+                        /* // column 1 contains getBadgeClassed() and formatCount() 
+                        // use {} to include somethings like js expression
+                        // getBadgeClasses return badge-warning when value is zero, otherwise 
+                        it returns badge-primary
+                        // formatCount returns zero when value is 0, otherwise returns that value
+                        // value of getBadgeClasses is assigned to a span's class, and formatCount 
+                        is assigned to span's text */
+                    }
+                    <div className="col-sm-2 py-2 ">
+                        <span className={this.getBadgeClasses()}>
+                            Number: {this.formatCount()}</span>
+                    </div>
+                    <div className="col-sm pl-0">
+                        {/* 
                             when u click on the + button, it calls the onIncrement(from props
                             ) method 
                             with the counter as an parameter(from props too) 
@@ -35,34 +36,35 @@ class Counter extends Component {
                             delete button ..., receive an counter.id as an parameter
 
                     */}
-                    <button onClick={() => this.props.onIncrement(this.props.counter)}
-                        className="btn btn-secondary btn-sm">+</button>
-                    <button onClick={() => this.props.onDecrement(this.props.counter)}
-                        className="btn btn-secondary btn-sm m-2" disabled=
-                        {this.props.counter.value === 0 ? "disabled" : ""}
-                    >-</button>
-                    <button
-                        className="btn btn-danger btn-sm"
-                        onClick={() => this.props.onDelete(this.props.counter.id)}
-                    >x
-                    </button>
+                        <button onClick={() => this.props.onIncrement(this.props.counter)}
+                            className="btn btn-secondary btn-sm"><i className="fas fa-plus-circle"></i></button>
+                        <button onClick={() => this.props.onDecrement(this.props.counter)}
+                            className="btn btn-secondary btn-sm m-2" disabled=
+                            {this.props.counter.value === 0 ? "disabled" : ""}
+                        ><i className="fas fa-minus-circle"></i></button>
+                        <button
+                            className="btn btn-danger btn-sm"
+                            onClick={() => this.props.onDelete(this.props.counter.id)}
+                        ><i className="far fa-trash-alt"></i>
+                        </button>
+                    </div>
+
+
+
                 </div>
-
-
-
             </div>
         );
     }
 
     getBadgeClasses() {
-        let classes = "badge m-1 badge-";
-        classes += this.props.counter.value === 0 ? "warning" : "primary";
+        let classes = "badge w-100 px-3 rounded badge-";
+        classes += this.props.counter.value === 0 ? "warning" : "success";
         return classes;
     }
 
     formatCount() {
         const { value } = this.props.counter;
-        return value === 0 ? "Zero" : value;
+        return value === 0 ? "zero" : value;
     }
 }
 export default Counter; // cc 
